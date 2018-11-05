0bd14e712e541c00d237be78bdb8d125dd88376c
Marianela <marianela_min@yahoo.com>
2018-11-05 15:37:22 -0600

    changed to redirect to server1.php which will not be tracked, as sensitive information might be written there

--- server.php
+++ server.php
@@ -1,21 +1,2 @@
 <?php
-
-//$ip_address = "165.124.51.197";
-$ip_address = "localhost";
-$username = "root";
-$password = "root";
-$database_name = "test_DB";
-$email = "";
-//$port = "3306";
-
-$db = mysqli_connect($ip_address, $username, $password, $database_name);
-if (!$db) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
-
-echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL."</br>";
-?>
+    header("Location: server1.php");
