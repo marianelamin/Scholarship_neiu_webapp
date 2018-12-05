7905b16a47ff45fc992d45e1a6a281e66ed6d751
Marianela <marianela_min@yahoo.com>
2018-12-04 23:52:24 -0600

    started the logic for loging in.. still need more

--- edit/login/login.php
+++ edit/login/login.php
@@ -11,34 +11,23 @@ if ( isset($_POST['cancel'] ) ) {
 $failure = false;  // If we have no POST data
 
 // Check to see if we have some POST data, if we do process it
-if ( isset($_POST['who']) ) {
-    if ( strlen($_POST['who']) != 6) {
-        $failure = "6-digit-number is required";
-    } else {
-        $var = urlencode($_POST['who']);
-            // echo $var."<br />";
-        $i = 0;
-        foreach ($nameFromId as $id) {
-            // echo $id."<br />";
-            $i++;
-            // echo $i;
-            if ($id == $var) {
-                // echo "Move to the page and start uploading...";
-                if(isset($_POST['name']) && strlen($_POST['name'])>=3 )
-                    {header("Location: upload.php?name=".urlencode($_POST['name'])."&id=".urldecode($_POST['who']));
-                }else{
-                    // echo "number of array: ".$i."id: ".$id."  name: ".$id_names[$i-1];
-                    // echo '<br/>';
-                    header("Location: upload.php?name=".$nameFromId[$var]."&id=".$var);
-                    }
-            return;
-            }else
-            {
-               $failure = "Incorrect number"; 
-            }
-        }
-      } 
-    }
+if ( !isset($_POST['email']) || !isset($_POST['password']) ) {
+    
+    $correo = htmlentities($_POST['email']);
+    $correo = htmlentities($_POST['password']);
+
+    // prepare conection to the db
+    // read the database, check for a user of that correo
+    //bring the row and check the hash against the password
+    //if correct show the edit page (start sesion)
+    //if not correct send back to log in (wipeout and destroy session).
+    
+    
+      }
+      else
+      {
+        $failure = "Incorrect email or password";
+      }
 
 
 // Fall through into the View
@@ -48,13 +37,13 @@ if ( isset($_POST['who']) ) {
 <html>
 <head><meta http-equiv="Content-Type" content="text/html; charset=euc-jp">
     <?php include("../css/style_config.php") ?>
-    <title>Iniciar Sesion - Proyecto de amor</title>
+    <title>Log In - Edit Scholarships</title>
 </head>
 <body>
 <div class="container">
 <div class="row justify-content-center">
 <div class="col-md-6 col-md-offset-3" align="center">
-<h1>NEIU</h1>
+<h1>Financial Aid - El Centro</h1>
 <h4>Please Log in</h4>
 
 <img src="../images/logo.png" alt=" logo"><br><br>
@@ -71,7 +60,7 @@ if ( $failure != "" )
     <input class="form-control" type="text" name="email" id="email" placeholder="Email..." required><span style="color:gray;"><?=$failure?></span><br/>
     <!-- <label for="pass">Contraseña: </label> -->
     <input class="form-control" type="password" name="password" id="pass" placeholder="Contraseña..." required><br/>
-    <input type="submit" name="submit" value="Inicia Sesion">
+    <input type="submit" name="submit" value="Log in">
     <input type="submit" name="cancel" value="Cancel">
 </form>
 <p>
