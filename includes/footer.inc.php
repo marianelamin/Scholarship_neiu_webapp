d1b1372b3ec00298d1f6269fec88e132f5f908ee
Marianela <marianela_min@yahoo.com>
2019-04-07 17:30:09 -0500

    these are files that have either variables or information that will likely be needed to include on different php files, ie: header, footer or constants, general styling etxc...

---
+++ includes/footer.inc.php
@@ -0,0 +1,35 @@
+<footer class="p-4">
+			<div class="container-fluid">
+				<div class="row align-items-center">
+					<h3>Hosted by <img alt="Northeastern Illinois University" class="img-fluid img-neiu-footer" src="images/neiu_wordmark_color.png" /> with the support of:</h3>
+				</div>
+
+				<div class="row align-items-center">
+					<div class="col-5 col-sm-3 col-md-2 col-lg-2">
+						<div class="small text-center pb-2">small text</div>
+					</div>
+					<span class="col col-sm col-md"></span>
+					<div class="col-12 col-sm-5 col-md-4 col-lg-5 text-center py-2">
+						<span class="text-center"> <a href="https://www.neiu.edu/academics/el-centro/" target="_blank">Northeastern Illinois University El Centro</a></span>
+						<span class="text-center"><address>3390 North Avondale<br />Chicago, IL 60618</address></span>
+						<span class="text-center"><a href="mailto:STEMed@neiu.edu" style="opacity:0.6;">STEMed@neiu.edu</a></span>
+					</div>
+				</div>
+			</div>
+		</footer>
+
+		<div class="container-fluid px-3">
+			<div class="row align-items-strech copyrights">
+				<div class="col-6 text-center"><span>Designed and developed by M&J Crissman</span></div>
+
+				<div class="col-6 text-center"><span>Copyrights 2019</span></div>
+			</div>
+		</div>
+
+
+
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	</body>
+	</html>
